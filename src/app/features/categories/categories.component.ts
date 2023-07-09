7de7ab00f1222b6455b8003a5ac027b6fa610cef
jajkiewi <izabela.jajkiewicz@gmail.com>
2023-07-09 10:47:31 +0200

    Dodawanie kategorii

--- src/app/features/categories/categories.component.ts
+++ src/app/features/categories/categories.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import {Category} from "../../models/category";
 
 @Component({
   selector: 'app-categories',
@@ -8,4 +9,15 @@ import {faPlus} from "@fortawesome/free-solid-svg-icons";
 })
 export class CategoriesComponent {
   plusIcon = faPlus;
+
+  categories: Category[] = [{name: "Sport"}, {name: "Dom"}];
+  // inicjalizacja zmiennej. Użytkownik będzie mógł dodawać nową wartość
+  categoryName = "";
+
+//  Metoda dodająca kategorie. Po "push" dajemy "name" - nazwę klucza
+  addCategory() {
+    this.categories.push({name: this.categoryName})
+  //  czyszczenie wartości
+    this.categoryName = ""
+  }
 }
