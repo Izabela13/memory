bd87e18e27b2ac45504afd8fe296418d0ee9dce3
jajkiewi <izabela.jajkiewicz@gmail.com>
2023-07-09 16:57:35 +0200

    Tryby gry

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {CategoriesComponent} from "./features/categories/categories.component";
 import {FlashCardsComponent} from "./features/flash-cards/flash-cards.component";
 import {GamesComponent} from "./features/games/games.component";
+import {WriteModeComponent} from "./features/games/write-mode/write-mode.component";
 
 const routes: Routes = [
   // Jeżeli wejdziemy na pustą ścieżkę, to przeniesie nas od razu do kategorii
@@ -11,7 +12,9 @@ const routes: Routes = [
   {path: "categories", component: CategoriesComponent},
   // po "category-id" będziemy wyciągali wartość
   {path: "categories/:category-id/flash-cards", component:FlashCardsComponent},
-  {path: "categories/:category-id/games", component:GamesComponent}
+  {path: "categories/:category-id/games", component:GamesComponent},
+  {path: "categories/:category-id/games/flashcard", component: FlashCardsComponent},
+  {path: "categories/:category-id/games.write", component: WriteModeComponent}
 ];
 
 @NgModule({
